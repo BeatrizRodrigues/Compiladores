06c5cf80ad6650baad07ff5da064d5672ca91b40
Beatriz Aparecida de Freitas Rodrigues <beeatrizz29@gmail.com>
2021-06-15 12:14:41 -0300

    Atividade para presença

--- src/Postfix.java
+++ src/Postfix.java
@@ -1,4 +1,42 @@
 
 public class Postfix {
+	static int lookahead;
+    
+    public Postfix() throws IOException{
+        lookahead = System.in.read();
+    }
 
+    void match(int t) throws IOException{
+        if(lookahead == t){
+            lookahead = System.in.read();
+        }
+        else{
+            throw new Error("** Syntax Error! ***");
+        }
+    }
+    
+    void term() throws IOException{
+        if(Character.isDigit((char)lookahead)||(char)lookahead==' '){
+            System.out.write((char)lookahead);
+            match(lookahead);
+        }
+        else{
+            throw new Error("** Syntax Error! ***");
+        }
+    }
+    
+    void expr() throws IOException{
+        term();
+        while(true){
+        	if(lookahead == '+') {
+        		match('+');
+                term();
+                System.out.write('+');
+        	}else if(lookahead == '-') {
+        		match('-');
+                term();
+                System.out.write('-');
+        	}
+        }
+    }
 }
